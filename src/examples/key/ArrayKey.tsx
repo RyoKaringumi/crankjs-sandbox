ebb93440965394ee4ea06d001ead6cd33afc839d
RyoKaringumi <ryo@ezoe.net>
2026-01-18 01:06:12 +0900

    ArrayKeyコンポーネントを追加し、KeyPageコンポーネントに統合

---
+++ src/examples/key/ArrayKey.tsx
@@ -0,0 +1,56 @@
+/** @jsxImportSource @b9g/crank */
+
+import type { Context } from "@b9g/crank";
+
+type ListItemType = {
+	id: string;
+	name: string;
+}
+
+function *Item(this:Context<typeof Item>, props:{item:ListItemType, items:ListItemType[],onChange:()=>void}){
+	console.log("Rendering Item", props.item.id);
+	for(props of this){
+		console.log("Re-Rendering Item", props.item.id);
+		const {item, items, onChange} = props;
+		yield (
+			<li key={item.id}><button  onClick={()=>{
+				// 下の要素と入れかえる
+				const index = items.findIndex(i=>i.id === item.id);
+				if(index < items.length - 1){
+					const temp = items[index + 1];
+					items[index + 1] = items[index];
+					items[index] = temp;
+				}
+				// 再レンダリング
+				// Reactと違い、mutableな操作でも問題なく、refreshを呼び出せばよい
+				onChange();
+				console.log("Swapped", item.id);
+			}}>Swap with next
+			
+			</button>{item.name}</li>
+		);
+	}
+}
+
+export default function *ArrayKey(this:Context){
+	const items: ListItemType[] = [
+		{id: "a", name: "Item A"},
+		{id: "b", name: "Item B"},
+		{id: "c", name: "Item C"},
+	];
+	const onChange = ()=>{this.refresh();};
+	for({} of this){
+		yield (
+			<div>
+			ArrayKey Example
+			<br/>
+			配列をKeyにした場合の挙動を確認するためのコンポーネントです。
+			<ul>
+			{items.map(item=>(
+				<Item key={item.id} item={item} items={items} onChange={onChange}/>
+			))}
+			</ul>
+			</div>
+		);
+	}
+}
--- src/examples/key/KeyPage.tsx
+++ src/examples/key/KeyPage.tsx
@@ -1,11 +1,14 @@
 /** @jsxImportSource @b9g/crank */
 
+import ArrayKey from "./ArrayKey";
+
 export default function KeyPage(){
 	return (
 		<div>
 			KeyPage
 			<br/>
 			Keyが存在するかや、ReactのKeyとの実装の違いを見るためのページです。
+			<ArrayKey/>
 		</div>
 	);
 }
